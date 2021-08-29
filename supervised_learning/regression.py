8c9844ea308120f9bc151ba49ab17f6aaa74c555
juan190199 <juanmanueldasilvatome@gmail.com>
2021-08-29 17:13:53 +0200

    ~// Regression class

--- supervised_learning/regression.py
+++ supervised_learning/regression.py
@@ -32,63 +32,63 @@ class L2_Regularization():
 
 
 class Regression(object):
-    # """
-    # Base regression model. Models the relationship between a scalar dependent variable y and the independent
-    # variables X.
-    #
-    # """
-    # def __init__(self, n_iterations, learning_rate):
-    #     self.n_iterations = n_iterations
-    #     self.learning_rate = learning_rate
-    #
-    # def initialize_weights(self, n_features, scale=1):
-    #     """
-    #     Initialize weights randomly
-    #
-    #     :param n_features:
-    #
-    #     :param scale: float
-    #         scale of np.random.normal
-    #     :return:
-    #     """
-    #     limit = 1 / math.sqrt(n_features)
-    #     self.w = np.random.normal(loc=0, scale=scale,  sizze=(n_features, ))
-    #
-    # def fit(self, X, y):
-    #     """
-    #
-    #     :param X: ndarray of shape (n_samples, n_features)
-    #         Training data
-    #     :param y: ndarray of shape (n_samples, )
-    #         Target data
-    #     :return:
-    #     """
-    #     # Insert constant ones for bias weights
-    #     X = np.insert(X, 0, 1, axis=1)
-    #     self.training_errors = []
-    #     self.initialize_weights(n_features=X.shape[1])
-    #
-    #     # Gradient descent for n_iterations
-    #     for i in range(self.n_iterations):
-    #         y_pred = X.dot(self.w)
-    #         # Calculate L2 loss w.r.t. w
-    #         mse = np.mean(0.5 * (y - y_pred) ** 2 + self.regularization(self.w))
-    #         self.training_errors.append(mse)
-    #         # Gradient of L2 loss w.r.t. w
-    #         grad_w = -(y - y_pred).dot(X) + self.regularization.grad(self.w)
-    #         # Update the weights
-    #         self.w -= self.learning_rate * grad_w
-    #
-    # def predict(self, X):
-    #     """
-    #
-    #     :param X:
-    #     :return:
-    #     """
-    #     #Insert constant ones for bias weights
-    #     X = np.insert(X, 0, 1, axis=1)
-    #     y_pred = X.dot(self.w)
-    #     return y_pred
+    """
+    Base regression model. Models the relationship between a scalar dependent variable y and the independent
+    variables X.
+
+    """
+    def __init__(self, n_iterations, learning_rate):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+
+    def initialize_weights(self, n_features, scale=1):
+        """
+        Initialize weights randomly
+
+        :param n_features:
+
+        :param scale: float
+            scale of np.random.normal
+        :return:
+        """
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.normal(loc=0, scale=scale,  sizze=(n_features, ))
+
+    def fit(self, X, y):
+        """
+
+        :param X: ndarray of shape (n_samples, n_features)
+            Training data
+        :param y: ndarray of shape (n_samples, )
+            Target data
+        :return:
+        """
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1])
+
+        # Gradient descent for n_iterations
+        for i in range(self.n_iterations):
+            y_pred = X.dot(self.w)
+            # Calculate L2 loss w.r.t. w
+            mse = np.mean(0.5 * (y - y_pred) ** 2 + self.regularization(self.w))
+            self.training_errors.append(mse)
+            # Gradient of L2 loss w.r.t. w
+            grad_w = -(y - y_pred).dot(X) + self.regularization.grad(self.w)
+            # Update the weights
+            self.w -= self.learning_rate * grad_w
+
+    def predict(self, X):
+        """
+
+        :param X:
+        :return:
+        """
+        #Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
 
 
 class LinearRegression(Regression):
