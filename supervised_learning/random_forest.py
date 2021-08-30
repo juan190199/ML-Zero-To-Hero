fdc697a8a9c8239a682234b2afff2476a32a8f4e
juan190199 <juanmanueldasilvatome@gmail.com>
2021-08-30 19:59:50 +0200

    ~// Blueprint RandomForest class

---
+++ supervised_learning/random_forest.py
@@ -0,0 +1,35 @@
+import math
+import numpy as np
+
+
+class RandomForest():
+    """
+
+    """
+    def __init__(self, n_estimators=100, max_features=None, min_samples_split=2, min_gain=0, max_depth=float('inf')):
+        """
+
+        :param n_estimators:
+        :param max_features:
+        :param min_samples_split:
+        :param min_gain:
+        :param max_depth:
+        """
+        ...
+
+    def fit(self, X, y):
+        """
+
+        :param X:
+        :param y:
+        :return:
+        """
+        ...
+
+    def predict(self, X):
+        """
+
+        :param X:
+        :return:
+        """
+        ...
