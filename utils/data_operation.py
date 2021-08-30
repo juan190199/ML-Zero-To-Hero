95f6c32d1bfa4d9c50b1b5e5f5c9a6a373645463
juan190199 <juanmanueldasilvatome@gmail.com>
2021-08-30 10:38:33 +0200

    ~// Implementation calculate_covariance_matrix method

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -12,3 +12,18 @@ def euclidean_distance(x1, x2):
     """
     distance = np.sqrt(np.sum(np.square(np.subtract(x1[:, None, :], x2)), axis=2))
     return distance
+
+
+def calculate_covariance_matrix(X, Y=None):
+    """
+
+    :param X:
+    :param Y:
+    :return:
+    """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples - 1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+
+    return np.array(covariance_matrix, dtype=float)
