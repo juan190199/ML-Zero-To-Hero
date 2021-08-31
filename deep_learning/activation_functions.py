81a75f6bc4c6788aca393e5b074e2c4493e97316
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-01 00:37:32 +0200

    ~// Implementation SoftMax activation function class

--- deep_learning/activation_functions.py
+++ deep_learning/activation_functions.py
@@ -5,6 +5,7 @@ Collection of activation functions
 Reference: https://en.wikipedia.org/wiki/Activation_function
 """
 
+
 class Sigmoid():
     def __call__(self, x):
         return 1 / (1 + np.exp(-x))
@@ -14,7 +15,13 @@ class Sigmoid():
 
 
 class Softmax():
-    ...
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)
 
 
 class TanH():
