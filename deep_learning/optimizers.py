49ac463e2d8420e6d084811b7b6684712a35df7c
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-02 13:26:29 +0200

    ~// Implementation Adagrad class

--- deep_learning/optimizers.py
+++ deep_learning/optimizers.py
@@ -33,4 +33,20 @@ class NesterovAcceleratedGradient():
         return w - self.w_update
 
 
+class Adagrad():
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.G = None  # Sum of squares of the gradients
+        self.eps = 1e-8
+
+    def update(self, w, grad_wrt_w):
+        # If not initialized
+        if self.G is None:
+            self.G = np.zeros(np.shape(w))
+        # Add the square of the gradient of the loss functions at w
+        self.G += np.power(grad_wrt_w, 2)
+        # Adaptive gradient with higher learning rate for sparse data
+        return w - self.learning_rate * grad_wrt_w / np.sqrt(self.G + self.eps)
+
+
 
