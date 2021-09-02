24735c5a50255a076b312ff2b3bf0d45252e6a6c
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-02 13:26:10 +0200

    ~// Implementation NesterovAccelerateGradient class

--- deep_learning/optimizers.py
+++ deep_learning/optimizers.py
@@ -16,4 +16,21 @@ class StochasticGradientDescent():
         return w - self.learning_rate * self.w_updt
 
 
+class NesterovAcceleratedGradient():
+    def __init__(self, learning_rate=0.001, momentum=0.4):
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.w_update = np.array([])
+
+    def update(self, w, grad_func):
+        # Calculate the gradient of the loss a bit further down the slope from w
+        approx_future_grad = np.clip(grad_func(w - self.momentum * self.w_update), -1, 1)
+        # Initialize on first update
+        if not self.w_update.any():
+            self.w_update = np.zeros(np.shape(w))
+
+        self.w_update = self.momentum * self.w_update * self.learning_rate * approx_future_grad
+        return w - self.w_update
+
+
 
