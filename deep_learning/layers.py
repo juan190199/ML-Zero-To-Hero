791d04b861d400fef3f91dc0227824c3589a76a8
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-05 12:17:45 +0200

    ~// get_im2col_indices method for Conv2D to calculate indices where the dot product between image and weights is gonna be applied

--- deep_learning/layers.py
+++ deep_learning/layers.py
@@ -325,17 +325,31 @@ def determine_padding(filter_shape, output_shape='same'):
         return (pad_h1, pad_h2), (pad_w1, pad_w2)
 
 
-def get_im2col_indices(image_shape, filter_shape, padding, stride=1):
+def get_im2col_indices(images_shape, filter_shape, padding, stride=1):
     """
 
-    :param image_shape:
+    :param images_shape:
     :param filter_shape:
     :param padding:
     :param stride:
     :return:
     """
     # First figure out what the size of the output should be
-    ...
+    batch_size, channels, height, width = images_shape
+    filter_height, filter_width = filter_shape
+    pad_h, pad_w = padding
+    out_height = int((height + np.sum(pad_h) - filter_height) / stride + 1)
+    out_width = int((width + np.sum(pad_w) - filter_width) / stride + 1)
+
+    i0 = np.repeat(np.arange(filter_height), filter_width)
+    i0 = np.tile(i0, channels)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(filter_width), filter_height * channels)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+    k = np.repeat(np.arange(channels), filter_height * filter_width).reshape(-1, 1)
 
 
 def image_to_column(images, filter_shape, stride, output_shape='same'):
