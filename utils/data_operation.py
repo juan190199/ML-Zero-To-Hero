1538c9558f1948414c598b08e2b6c49b7164d690
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-12 00:26:49 +0200

    ~// Mean squared error

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -2,6 +2,18 @@ import math
 import numpy as np
 
 
+def mean_squared_error(y_true, y_pred):
+    """
+    Returns the mean squared error between y_true and y_pred
+
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    mse = np.mean(np.power(y_true - y_pred, 2))
+    return mse
+
+
 def calculate_variance(X):
     """
 
