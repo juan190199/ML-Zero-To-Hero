891567cd24e38854629667e752056855014de42c
juan190199 <juanmanueldasilvatome@gmail.com>
2021-09-14 14:12:22 +0200

    ~// Implementation Flatten layer class

--- deep_learning/layers.py
+++ deep_learning/layers.py
@@ -439,6 +439,26 @@ class ZeroPadding2D(ConstantPadding2D):
         self.padding_value = 0
 
 
+class Flatten(Layer):
+    """
+    Turns multi-dimensional matrix into two-dimensional
+    """
+    def __init__(self, input_shape=None):
+        self.prev_shape = None
+        self.trainable = True
+        self.input_shape = input_shape
+
+    def forward_pass(self, X, training=True):
+        self.prev_shape = X.shape
+        return X.reshape((X.shape[0], -1))
+
+    def backward_pass(self, accum_grad):
+        return accum_grad.reshape(self.prev_shape)
+
+    def output_shape(self):
+        return (np.prod(self.input_shape), )
+
+
 class RNN(Layer):
     """
     A Vanilla Fully-Connected Recurrent Neural Network layer
