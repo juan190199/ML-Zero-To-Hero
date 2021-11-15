be6d75f978afb871eddd82aa0e88ad7e91babcb9
juan190199 <juanmanueldasilvatome@gmail.com>
2021-11-15 23:08:27 +0100

    ~// Bug when calculating square loss

--- deep_learning/loss_functions.py
+++ deep_learning/loss_functions.py
@@ -18,7 +18,7 @@ class SquareLoss(Loss):
     def __init__(self): pass
 
     def loss(self, y, y_pred):
-        return 0.5 * np.power((y - y_pred), axis=2)
+        return 0.5 * np.power((y - y_pred), 2)
 
     def gradient(self, y, y_pred):
         return -(y - y_pred)
