8b9bca0a7bbfcfcbea61e5d8d9de0a17cceafb5e
juan190199 <juanmanueldasilvatome@gmail.com>
2021-11-16 23:10:39 +0100

    ~// Return loss and accuracy in test_on_batch

--- deep_learning/neural_network.py
+++ deep_learning/neural_network.py
@@ -49,7 +49,7 @@ class NeuralNetwork():
         # If not first hidden layer,
         # then set the input shape to the output shape of the last added layer.
         if self.layers:
-            layer.set_input.shape(shape=self.layers[-1].output.shape())
+            layer.set_input_shape(shape=self.layers[-1].output_shape())
 
         # If the layer has weights that need to be initialized
         if hasattr(layer, 'initialize'):
@@ -65,9 +65,11 @@ class NeuralNetwork():
         :param y:
         :return:
         """
-        y_pred = self._forward_pass(X)
+        y_pred = self._forward_pass(X, training=False)
         loss = np.mean(self.loss_function.loss(y, y_pred))
         acc = self.loss_function.acc(y, y_pred)
+
+        return loss, acc
 
     def train_on_batch(self, X, y):
         """
@@ -96,10 +98,19 @@ class NeuralNetwork():
         :return:
         """
         for _ in self.progressbar(range(n_epochs)):
+
             batch_error = []
             for X_batch, y_batch in batch_iterator(X, y, batch_size=batch_size):
                 loss, _ = self.train_on_batch(X_batch, y_batch)
                 batch_error.append(loss)
+
+        #     self.errors["training"].append(np.mean(batch_error))
+        #
+        #     if self.val_set is not None:
+        #         val_loss, _ = self.test_on_batch(self.val_set["X"], self.val_set["y"])
+        #         self.errors["validation"].append(val_loss)
+        #
+        # return self.errors["training"], self.errors["validation"]
 
     def _forward_pass(self, X, training=True):
         """
@@ -111,6 +122,8 @@ class NeuralNetwork():
         layer_output = X
         for layer in self.layers:
             layer_output = layer.forward_pass(layer_output, training)
+
+        # return layer_output
 
     def _backward_pass(self, loss_grad):
         """
