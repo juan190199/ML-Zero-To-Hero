063d938dc8102d3cc0c6c2b7096974710a717dfc
juan190199 <juanmanueldasilvatome@gmail.com>
2022-05-14 17:39:45 +0200

    ~// Implementation KFold method to obtain all kFold cross validation sets (X_train, X_test, y_train, y_test)

--- utils/data_manipulation.py
+++ utils/data_manipulation.py
@@ -172,3 +172,43 @@ def make_diagonal(x):
     for i in range(len(m[0])):
         m[i, i] = x[i]
     return m
+
+
+def KFold(X, y, k, shuffle=True):
+    """
+
+    :param X:
+    :param y:
+    :param k:
+    :param shuffle:
+    :return:
+    """
+    if shuffle:
+        X, y = shuffle_data(X, y)
+
+    n_samples = len(y)
+    left_overs = {}
+    n_left_overs = (n_samples % k)
+    if n_left_overs != 0:
+        left_overs['X'] = X[-n_left_overs:]
+        left_overs['y'] = y[-n_left_overs:]
+        X = X[:-n_left_overs]
+        y = y[:-n_left_overs]
+
+    X_split = np.split(X, k)
+    y_split = np.split(y, k)
+
+    sets = []
+    for i in range(k):
+        X_test, y_test = X_split[i], y_split[i]
+        X_train = np.concatenate(X_split[:i] + X_split[i + 1:], axis=0)
+        y_train = np.concatenate(y_split[:i] + y_split[i + 1:], axis=0)
+        sets.append([X_train, X_test, y_train, y_test])
+
+    # Add left over samples to last set as training samples
+    if n_left_overs != 0:
+        np.append(sets[-1][0], left_overs['X'], axis=0)
+        np.append(sets[-1][2], left_overs['y'], axis=0)
+
+    return np.array(sets)
+
