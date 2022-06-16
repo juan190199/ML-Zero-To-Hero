7ecaffee05aeac42f3d2bd03d2dccb647a3cecf0
juan190199 <juanmanueldasilvatome@gmail.com>
2022-06-16 10:22:43 +0200

    ~// ExponentialFamilyMixin containing methodds common for all ExponentialFamily objects

--- utils/families.py
+++ utils/families.py
@@ -3,11 +3,36 @@ import numpy as np
 
 
 class ExponentialFamily(metaclass=ABCMeta):
-    ...
+    """
+    An exponential family must implement at least four methods and define one attribute
+    """
+    @abstractmethod
+    def inv_link(self, nu):
+        pass
+
+    @abstractmethod
+    def d_inv_link(self, nu, mu):
+        pass
+
+    @abstractmethod
+    def variance(self, mu):
+        pass
+
+    @abstractmethod
+    def deviance(self, y, mu):
+        pass
+
+    @abstractmethod
+    def sample(self, mus, dispersion):
+        pass
 
 
 class ExponentialFamilyMixin:
-    ...
+    """
+    Implementation of methods common to all ExponentialFamily objects
+    """
+    def penalized_deviance(self, y, mu, alpha, coef):
+        return self.deviance(y, mu) + alpha * np.sum(coef[1:] ** 2)
 
 
 class Gaussian(ExponentialFamily, ExponentialFamilyMixin):
