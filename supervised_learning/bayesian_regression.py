53b784a7aaa84a2c493c689b068462264cdd56d2
juan190199 <juanmanueldasilvatome@gmail.com>
2022-06-30 14:22:24 +0200

    ~// fit method Bayesian regression

--- supervised_learning/bayesian_regression.py
+++ supervised_learning/bayesian_regression.py
@@ -43,3 +43,51 @@ class BayesianRegression(object):
         self.nu0 = nu0
         self.sigma_sq0 = sigma_sq0
 
+    def _draw_scaled_inv_chi_sq(self, n, df, scale):
+        X = chi2.rvs(size=n, df=df)
+        sigma_sq = df * scale / X
+        return sigma_sq
+
+    def fit(self, X, y):
+        """
+
+        :param X:
+        :param y:
+        :return:
+        """
+        # If polynomial transformation
+        if self.poly_degree:
+            X = polynomial_features(X, degree=self.poly_degree)
+
+        n_samples, n_features = np.shape(X)
+
+        X_X = X.T.dot(X)
+
+        # Least squares approximate of theta
+        beta_hat = np.linalg.pinv(X_X).dot(X.T).dot(y)
+
+        # The posteriors can be computed analytically since we assume conjugate priors for the likelihood.
+        # Normal prior / likelihood => Normal posterior
+        omega_n = X_X + self.omega0
+        mu_n = np.linalg.pinv(omega_n).dot(X_X.dot(beta_hat) + self.omega0.dot(self.mu0))
+        # Scaled inverse chi-squared prior / likelihood => Scaled inverse chi-squared posterior
+        nu_n = self.nu0 + n_samples
+        sigma_sq_n = (1.0 / nu_n) * (self.nu0 * self.sigma_sq0 + (
+                y.T.dot(y) + self.mu0.T.dot(self.omega0).dot(self.mu0) - mu_n.T.dot(omega_n.dot(mu_n))))
+
+        # Simulate parameter values for n_draws
+        beta_draws = np.empty((self.n_draws, n_features))
+        for i in range(self.n_draws):
+            sigma_sq = self._draw_scaled_inv_chi_sq(n=1, df=nu_n, scale=sigma_sq_n)
+            beta = multivariate_normal.rvs(size=1, mean=mu_n[:, 0], cov=sigma_sq * np.linalg.pinv(omega_n))
+            # Save parameter draws
+            beta_draws[i, :] = beta
+
+        # Select the mean of the simulated variables as the ones used to make predictions
+        self.w = np.mean(beta_draws, axis=0)
+
+        # Lower and upper boundary of the credible interval
+        l_eti = 50 - self.cred_int / 2
+        u_eti = 50 + self.cred_int / 2
+        self.eti = np.array([[np.percentile(beta_draws[:, i], q=l_eti), np.percentile(beta_draws[:, i], q=u_eti)] \
+                             for i in range(n_features)])
