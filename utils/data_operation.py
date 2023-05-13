5a6f9301df7be516260c0a35656a93da43074f34
juan190199 <juanmanueldasilvatome@gmail.com>
2023-05-13 15:31:15 +0200

    ~// Implementation of soft thresholding operator

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -125,3 +125,12 @@ def rescale_data(X, y, sample_weight):
     y = safe_sparse_dot(sw_matrix, y)
 
     return X, y
+
+
+def soft_thresholding_operator(self, x, lambda_):
+    if x > 0 and lambda_ < abs(x):
+        return x - lambda_
+    elif x < 0 and lambda_ < abs(x):
+        return x + lambda_
+    else:
+        return 0.0
