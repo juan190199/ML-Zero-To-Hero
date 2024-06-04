c6d36f18f5b67b07ee6ee14ba1d6a02e6c36c5b1
Juan Da Silva <juan19.99@Juans-MacBook-Pro.local>
2024-06-04 17:23:26 +0200

    ~// Implementing Softmax Regression (that also includes binary classification) and deleting Logistic Regression

--- supervised_learning/linear_models/linear_classifiers.py
+++ supervised_learning/linear_models/linear_classifiers.py
@@ -2,58 +2,76 @@ import math
 import numpy as np
 
 from deep_learning import CrossEntropy
-from supervised_learning.linear_models.regression import LinearRegression, RidgeRegression
-from utils.data import make_diagonal
-from deep_learning.activation_functions import Sigmoid
+from supervised_learning.linear_models.regression import Regression, LinearRegression, RidgeRegression
+from utils.data import make_diagonal, normalize, polynomial_features, to_categorical
+from deep_learning.activation_functions import Sigmoid, Softmax
 
 
-class LogisticRegression(LinearRegression):
+class SoftmaxRegression(Regression):
     """
-    Logistic regression classifier
+    Softmax regression classifier
     """
+    # ToDo: implement features class weight,
+    #  solvers(lbfgs - multinomial, penalty(l2), newton-cg - multinomial, penalty(l2), newton-cholesky - penalty: l2,
+    #  saga - multinomial, penalty(elasticnet, l1, l2))
 
     def __init__(self, max_iterations=1000, learning_rate=0.01, tol=1e-4, solver='gradient_descent'):
-        super().__init__(max_iterations=max_iterations, learning_rate=learning_rate, solver=solver)
-        self.tol = tol
-        self.sigmoid = Sigmoid()
-        self.cross_entropy = CrossEntropy()
+        self.allowed_solvers = ["gradient_descent"]
+        self.degree = 1
+
+        self.regularization = lambda x: 0
+        self.regularization.grad = lambda x: 0
+
+        self.loss_function = CrossEntropy()
+
+        super().__init__(
+            max_iterations=max_iterations,
+            learning_rate=learning_rate,
+            tol=tol,
+            solver=solver
+        )
+
+    def initialize_weights(self, n_features, n_classes, scale=1):
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, size=(n_features, n_classes))
 
     def fit(self, X, y, sample_weight=None):
-        self.initialize_weights(X.shape[1])
+        X = normalize(polynomial_features(X, degree=self.degree))
+        X = np.insert(X, 0, 1, axis=1)
+        sample_weight = make_diagonal(np.ones(X.shape[0])) if sample_weight is None else make_diagonal(sample_weight)
+
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1], n_classes=len(np.unique(y)))
 
         if self.solver == 'gradient_descent':
-            self.gradient_descent(X, y)
-        elif self.solver == 'newton_raphson':
-            self.newton_raphson(X, y)
+            self.gradient_descent(
+                np.sqrt(sample_weight).dot(X),
+                np.sqrt(sample_weight).dot(to_categorical(y, len(np.unique(y))))
+            )
 
     def predict_proba(self, X):
-        return self.sigmoid(np.dot(X, self.w))
+        X = normalize(polynomial_features(X, degree=self.degree))
+        X = np.insert(X, 0, 1, axis=1)
+
+        logits = X.dot(self.w)
+        probabilities = Softmax()(logits)
+        return probabilities
 
     def predict(self, X):
-        return np.round(self.predict_proba(X)).astype(int)
+        probabilities = self.predict_proba(X)
+        return np.argmax(probabilities, axis=1)
 
     def gradient_descent(self, X, y):
+        n_samples, n_features = X.shape
         for i in range(self.max_iterations):
-            y_pred = self.sigmoid(np.dot(X, self.w))
-            loss = self.cross_entropy.loss(y, y_pred)
-            grad = self.cross_entropy.gradient(y, y_pred)
+            logits = X.dot(self.w)
+            probabilities = Softmax()(logits)
 
-            self.w -= self.learning_rate * np.dot(X.T, grad)
+            loss = self.loss_function.loss(y, probabilities)
+            self.training_errors.append(loss)
 
-            if np.linalg.norm(grad) < self.tol:
-                break
-
-    def newton_raphson(self, X, y):
-        for i in range(self.max_iterations):
-            y_pred = self.sigmoid(np.dot(X, self.w))
-            loss = self.cross_entropy.loss(y, y_pred)
-            grad = self.cross_entropy.gradient(y, y_pred)
-            diag_gradient = make_diagonal(self.sigmoid.gradient(X.dot(self.w)))
-            hessian = np.dot(X.T, np.dot(diag_gradient, X))
-            self.w -= np.linalg.inv(hessian).dot(np.dot(X.T, grad))
-
-            if np.linalg.norm(grad) < self.tol:
-                break
+            grad_w = np.dot(X.T, self.loss_function.gradient(y, probabilities))
+            self.w -= self.learning_rate * grad_w
 
 
 class RidgeClassifier(RidgeRegression):
