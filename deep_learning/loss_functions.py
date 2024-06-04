da61f0d15a007955a7ba64ab5f7e497e2f8cf17e
Juan Da Silva <juan19.99@Juans-MacBook-Pro.local>
2024-06-04 11:36:29 +0200

    ~// Implementation cross entropy for multi-class classification problems

--- deep_learning/loss_functions.py
+++ deep_learning/loss_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 sys.path.append('../')
 
-from utils import accuracy_score
+from utils import accuracy_score, to_categorical
 
 
 class Loss(object):
@@ -17,28 +17,60 @@ class Loss(object):
         return 0
 
 
+##################################
+###     Regression metrics     ###
+##################################
+
 class SquareLoss(Loss):
-    def __init__(self): pass
+    def __init__(self):
+        pass
 
-    def loss(self, y, y_pred):
-        return 0.5 * np.power((y - y_pred), 2)
+    def loss(self, y, y_pred, normalize=True):
+        if normalize:
+            return 0.5 * np.mean(np.power((y - y_pred), 2))
+        else:
+            return 0.5 * np.sum(np.power((y - y_pred), 2))
 
-    def gradient(self, y, y_pred):
-        return -(y - y_pred)
+    def gradient(self, y, y_pred, normalize=True):
+        if normalize:
+            return -(y - y_pred) / y.shape[0]
+        else:
+            return -(y - y_pred)
 
+
+######################################
+###     Classification metrics     ###
+######################################
 
 class CrossEntropy(Loss):
-    def __init__(self): pass
+    def __init__(self):
+        pass
 
-    def loss(self, y, y_pred):
+    def loss(self, y, y_pred, normalize=True):
+
+        # Ensure y is one-hot encoded
+        if len(y.shape) == 1 or y.shape[1] == 1:
+            y = to_categorical(y)
+
         # Avoid division by zero
         y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return -y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)
+        sample_losses = -np.sum(y * np.log(y_pred), axis=1)
+
+        if normalize:
+            return np.mean(sample_losses)
+        else:
+            return np.sum(sample_losses)
 
     def acc(self, y, y_pred):
         return accuracy_score(np.argmax(y, axis=1), np.argmax(y_pred, axis=1))
 
-    def gradient(self, y, y_pred):
-        # Avoid division by zero
-        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
-        return -(y / y_pred) + (1 - y) / (1 - y_pred)
+    def gradient(self, y, y_pred, normalize=True):
+        # Ensure y is one-hot encoded
+        if len(y.shape) == 1 or y.shape[1] == 1:
+            y = to_categorical(y)
+
+        grad = y_pred - y
+        if normalize:
+            return grad / y.shape[0]
+        else:
+            return grad
