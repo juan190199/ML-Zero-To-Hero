46918d84217c42374bf3bf8febfa2178a1a35bab
juan19.99 <juanmanueldasilvatome@gmail.com>
2024-06-27 14:59:23 +0200

    ~// Implementation ResNet architecture

--- deep_learning/__init__.py
+++
@@ -1,6 +0,0 @@
-from .layers import (Dense, Dropout, Conv2D, Activation, MaxPooling2D, AveragePooling2D, ZeroPadding2D)
-from .loss_functions import (SquareLoss, CrossEntropy)
-from .neural_network import NeuralNetwork
-from .optimizers import (StochasticGradientDescent, Adam, RMSprop, Adagrad, Adadelta)
-
-
