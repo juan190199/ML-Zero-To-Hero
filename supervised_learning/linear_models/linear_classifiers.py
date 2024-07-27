996258247b5aee0c167548d8bf42b30736c407bc
juan19.99 <juanmanueldasilvatome@gmail.com>
2024-07-27 10:45:54 +0200

    ~// Weight class in softmax regression

--- supervised_learning/linear_models/linear_classifiers.py
+++ supervised_learning/linear_models/linear_classifiers.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-from deep_learning import CrossEntropy
+from deep_learning.loss_functions import CrossEntropy
 from supervised_learning.linear_models.regression import Regression, LinearRegression, RidgeRegression
 from utils.data import make_diagonal, normalize, polynomial_features, to_categorical
 from deep_learning.activation_functions import Sigmoid, Softmax
@@ -11,18 +11,19 @@ class SoftmaxRegression(Regression):
     """
     Softmax regression classifier
     """
-    # ToDo: implement features class weight,
-    #  solvers(lbfgs - multinomial, penalty(l2), newton-cg - multinomial, penalty(l2), newton-cholesky - penalty: l2,
+    #  solvers: lbfgs - multinomial, penalty(l2), newton-cg - multinomial, penalty(l2), newton-cholesky - penalty: l2,
     #  saga - multinomial, penalty(elasticnet, l1, l2))
 
-    def __init__(self, max_iterations=1000, learning_rate=0.01, tol=1e-4, solver='gradient_descent'):
+    def __init__(self, max_iterations=1000, learning_rate=0.01, tol=1e-4, solver='gradient_descent', class_weights=None):
         self.allowed_solvers = ["gradient_descent"]
         self.degree = 1
 
         self.regularization = lambda x: 0
         self.regularization.grad = lambda x: 0
 
-        self.loss_function = CrossEntropy()
+        self.class_weights = class_weights
+
+        self.loss_function = CrossEntropy(class_weights=self.class_weights)
 
         super().__init__(
             max_iterations=max_iterations,
